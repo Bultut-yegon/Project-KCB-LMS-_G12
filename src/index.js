d5b0f0579a34c40a6fb53727435377f14e110a78
Bultut-yegon <bultut9785@gmail.com>
2024-02-03 16:47:00 +0300

    debugs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth'
 import {getDatabase} from 'firebase/database'
 
+const appSettings = 'https://kcb-lms-default-rtdb.firebaseio.com/'
 const firebaseConfig = {
   apiKey: "AIzaSyBbMbTOH3NHv6fp4jAT3HA3vA6-Ur23In4",
   authDomain: "group-12-kcb-lms.firebaseapp.com",
@@ -10,7 +11,7 @@ const firebaseConfig = {
   messagingSenderId: "15476877993",
   appId: "1:15476877993:web:1c45be65570b120d48d7b8"
 };
-const app= initializeApp(firebaseConfig)
+const app= initializeApp(appSettings)
 
 export const auth= getAuth(app)
 
